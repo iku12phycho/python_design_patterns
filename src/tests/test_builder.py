c77350ac417208e30b260ca2802809e0c5e9b14c
Kensuke Ikunaga <iku12phycho@gmail.com>
2022-07-20 06:32:11 +0900

    design pattern builder

---
+++ src/tests/test_builder.py
@@ -0,0 +1,21 @@
+from Builder import builder
+
+def test_director_htmlbuilder(capfd):
+  html = builder.Director(builder.HTMLBuilder())
+  print(html)
+  out, err = capfd.readouterr()
+  assert out == '<h1>Monthly Report</h1>\n'\
+                '<header><p>-------</p></header>\n'\
+                '<p>Monday: 20</p>\n'\
+                '<p>Tuesday: 30</p>\n'\
+                '<footer><p>-*-*-*-</p></footer>\n'
+
+def test_director_textbuilder(capfd):
+  text = builder.Director(builder.TextBuilder())
+  print(text)
+  out, err = capfd.readouterr()
+  assert out == '**Monthly Report**\n'\
+                '-------\n'\
+                'Monday: 20\n'\
+                'Tuesday: 30\n'\
+                '-*-*-*-\n'
